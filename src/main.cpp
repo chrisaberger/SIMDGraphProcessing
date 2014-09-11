e5455132cb65d13c6b89a3b2b901db5febe2d857
Christopher Aberger <craberger@gmail.com>
2014-09-10 18:38:27 -0700

    Ported over new structure to master.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,18 @@
 // class templates
-#include "Common.hpp"
+#include "structures/Matrix.cpp"
+#include "structures/Common.hpp"
 
 using namespace std;
 
 int main (int argc, char* argv[]) { 
-  prepare_shuffling_dictionary16();
-  unsigned int *a = new unsigned int[100];
-  unsigned int *b = new unsigned int[200];
-
-  for(size_t i = 0; i < 100; i++){
-    a[i] = i*4;
+  unsigned int *data = new unsigned int[65536];
+  for(size_t i=0; i < 65536; i++){
+    data[i] = i;
   }
-  for(size_t i = 0; i < 200; i++){
-    b[i] = i;
-  }
-
-  unsigned short *a_r = new unsigned short[500];
-  unsigned short *b_r = new unsigned short[500];
-  size_t a_i = partition(a,100,a_r,0);
-  size_t b_i = partition(b,100,b_r,0);
-
-  unsigned short *c_r = new unsigned short[500];
-  size_t c_i = intersect_partitioned(c_r,a_r,b_r,a_i,b_i);
-  print_partition(c_r,c_i);
-  return 0;
+  unsigned short *result = new unsigned short[65536];
+  unsigned short *output = new unsigned short[65536];
+  size_t index = bitset::preprocess(result,0,data,65536);
+  cout << "index: " << index << endl;
+  bitset::print_data(result,index);
+  cout << "Count: " << bitset::intersect(output,result,result,index,index) << endl;
 }

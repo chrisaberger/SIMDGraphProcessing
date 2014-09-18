b9f58a50d38428dbeb3bf6af2d2e76b2fdd257bd
Christopher Aberger <craberger@gmail.com>
2014-09-17 18:42:47 -0700

    Added in delta compression.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main (int argc, char* argv[]) { 
-	size_t mysize = 16;
+	size_t mysize = 18;
   unsigned int *data = new unsigned int[mysize];
   for(size_t i=0; i < mysize; i++){
   	cout << "i: " << i << " Data: " << i*65536 << endl;
--- src/structures/DeltaCompA32.hpp
+++ src/structures/DeltaCompA32.hpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define DELTA 1
+
 namespace deltacompa32 {
   inline size_t encode_array(unsigned int *data, unsigned int length, unsigned int *result){
     const unsigned int bits_used = ceil(log2(data[length-1]));
@@ -26,7 +28,12 @@ namespace deltacompa32 {
     while(num_packed < num_simd_packed){
       while(bit_i+bits_used <= 32 && data_i < num_simd_packed){
         data_register = _mm_loadu_si128((__m128i*)&data[data_i]);
-        __m128i cur = _mm_slli_epi32(data_register,bit_i);
+        #if DELTA == 1
+        __m128i dt = _mm_sub_epi32(data_register,prev_data_register);
+        #else
+        __m128i dt = data_register;
+        #endif
+        __m128i cur = _mm_slli_epi32(dt,bit_i);
         packed_register = _mm_or_si128(cur,packed_register);
 
         data_i += INTS_PER_REG;
@@ -76,17 +83,21 @@ namespace deltacompa32 {
 
     __m128i data_register;
     __m128i prev_result = _mm_set1_epi32(0);
-
+    size_t data_incrementer = 0;
     while(num_decoded < num_packed){
       cout << "Bit Index: " << bit_i << endl;
       while(bit_i+bits_used <= 32 && num_decoded < num_packed){
         data_register = _mm_loadu_si128((__m128i*)&data[data_i]);
-        
+        data_incrementer = 0;
         uint32_t *t = (uint32_t*) &data_register;
         cout << "LOADED Values[" << data_i << "]: " << t[0] << " " << t[1] << " " << t[2] << " " << t[3] << endl;
 
         __m128i cur = _mm_srli_epi32(data_register,bit_i);
         __m128i result = _mm_and_si128(cur,mask);
+
+        #if DELTA == 1
+        result = _mm_add_epi32(result,prev_result);
+        #endif
 
         uint32_t *tt = (uint32_t*) &result;
         cout << "1Decoded Values[" << num_decoded << "]: " << tt[0] << " " << tt[1] << " " << tt[2] << " " << tt[3] << endl;
@@ -99,14 +110,17 @@ namespace deltacompa32 {
       if(bit_i < 32 && num_decoded < num_packed){
         __m128i cur_lower = _mm_srli_epi32(data_register,bit_i);
         data_register = _mm_loadu_si128((__m128i*)&data[data_i+INTS_PER_REG]);
-
+        data_incrementer = 4;
         uint32_t *tt = (uint32_t*) &data_register;
         cout << "LOADED Values[" << data_i+INTS_PER_REG << "]: " << tt[0] << " " << tt[1] << " " << tt[2] << " " << tt[3] << endl;
 
         __m128i cur_upper = _mm_slli_epi32(data_register,(32-bit_i));
 
         __m128i result = _mm_or_si128(cur_upper,cur_lower);
-        
+        #if DELTA
+        result = _mm_add_epi32(result,prev_result);
+        #endif
+
         uint32_t *t = (uint32_t*) &result;
         cout << "2Decoded Values[" << num_decoded << "]: " << t[0] << " " << t[1] << " " << t[2] << " " << t[3] << endl;
 
@@ -118,10 +132,13 @@ namespace deltacompa32 {
       }
       data_i += INTS_PER_REG;
     }
+
+    data_i += data_incrementer;
     cout << "Data_I: " << data_i << " Length: " << length << endl;
     while(num_decoded < cardinality){
       cout << "Decoded Value: " << data[data_i] << endl;
       data_i++; num_decoded++;
     }
+    prev_result = prev_result;
   }
 } 

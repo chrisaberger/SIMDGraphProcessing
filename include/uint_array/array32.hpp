31cb2fe492e7c86db54ba19f6c097a4e1e8b74f6
Christopher Aberger <craberger@gmail.com>
2014-10-10 19:20:40 -0700

    Set union working and getting good perf compared to std.

--- include/uint_array/array32.hpp
+++ include/uint_array/array32.hpp
@@ -33,13 +33,13 @@ namespace array32 {
   static __m128i shuffle_difference_mask32_a[16] = {        
     _mm_set_epi8(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0), //0
     _mm_set_epi8(max,max,max,max,15,14,13,12,11,10,9,8,7,6,5,4), //1
-    _mm_set_epi8(max,max,max,max,11,10,9,8,7,6,5,4,3,2,1,0), //2
+    _mm_set_epi8(max,max,max,max,15,14,13,12,11,10,9,8,3,2,1,0), //2
     _mm_set_epi8(max,max,max,max,max,max,max,max,15,14,13,12,11,10,9,8), //3
     _mm_set_epi8(max,max,max,max,15,14,13,12,7,6,5,4,3,2,1,0), //4
     _mm_set_epi8(max,max,max,max,max,max,max,max,15,14,13,12,7,6,5,4), //5 0101
     _mm_set_epi8(max,max,max,max,max,max,max,max,15,14,13,12,3,2,1,0), //6 0110
     _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,15,14,13,12), //7
-    _mm_set_epi8(max,max,max,max,15,14,13,12,7,6,5,4,3,2,1,0), //8 1000
+    _mm_set_epi8(max,max,max,max,11,10,9,8,7,6,5,4,3,2,1,0), //8 1000
     _mm_set_epi8(max,max,max,max,max,max,max,max,11,10,9,8,7,6,5,4), //9 1001
     _mm_set_epi8(max,max,max,max,max,max,max,max,11,10,9,8,3,2,1,0), //10 1010
     _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,11,10,9,8), //11 1011
@@ -72,7 +72,7 @@ namespace array32 {
     _mm_set_epi8(max,max,max,max,0,0,0,0,0,0,0,0,0,0,0,0), //1
     _mm_set_epi8(max,max,max,max,0,0,0,0,0,0,0,0,0,0,0,0),//2
     _mm_set_epi8(max,max,max,max,max,max,max,max,0,0,0,0,0,0,0,0), //3
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,0,0,0,0), //4
+    _mm_set_epi8(max,max,max,max,0,0,0,0,0,0,0,0,0,0,0,0), //4
     _mm_set_epi8(max,max,max,max,max,max,max,max,0,0,0,0,0,0,0,0), //5
     _mm_set_epi8(max,max,max,max,max,max,max,max,0,0,0,0,0,0,0,0), //6
     _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,0,0,0,0), //7
@@ -203,9 +203,13 @@ namespace array32 {
       next_i_a = i_a + 4;
       next_i_b = i_b + 4;
 
-      cout << "printing data" << endl;
-      print_sse_register(v_a);
-      print_sse_register(v_b);
+      /*
+      if(count > 6460 && count < 6470){
+        cout << "Data:" << endl;
+        print_sse_register(v_a);
+        print_sse_register(v_b); 
+      } */
+      
 
       //[ compute mask of common elements
       unsigned int right_cyclic_shift = _MM_SHUFFLE(0,3,2,1);
@@ -235,7 +239,15 @@ namespace array32 {
       #if WRITE_VECTOR == 1
       __m128i p_a = _mm_or_si128(setinel_mask_a[mask_a],_mm_shuffle_epi8(v_a, shuffle_difference_mask32_a[mask_a]));  
       __m128i p_b = v_b;
-     
+  
+      /*
+      if(count > 6460 && count < 6470){
+        cout << "p's:" << endl;
+        cout << mask_a << endl;
+        print_sse_register(p_a);
+        print_sse_register(p_b); 
+      } */
+          
       __m128i l_1 = _mm_min_epu32(p_a,p_b);
       __m128i h_1 = _mm_max_epu32(p_a,p_b);      
 
@@ -261,34 +273,38 @@ namespace array32 {
       __m128i h_4 = _mm_max_epu32(h_3,_mm_shuffle_epi32(l_3,right_cyclic_shift));
       _mm_storeu_si128((__m128i*)&C[count], l_4);
       _mm_storeu_si128((__m128i*)&C[count+INTS_PER_REG], h_4);
-
-      cout <<"Num hit: " << num_hit << endl;
-      print_sse_register(l_4);
-      print_sse_register(h_4);
-
+      
       #endif
 
       unsigned int new_a_max = a_max;
-      cout << "count: " << count << endl;
-      cout << "maxes: " << a_max << " " << b_max << " " << i_a << " " << next_i_a << endl;
-      if(a_max <= b_max){
+      if(a_max < b_max){
         i_a = next_i_a;
         v_a = _mm_loadu_si128((__m128i*)&A[i_a]);
         new_a_max = _mm_extract_epi32(v_a, 3);
         v_b = _mm_loadu_si128((__m128i*)&C[count+(INTS_PER_REG-num_hit)]);
-      }
-      if(a_max >= b_max){
+        count += INTS_PER_REG-num_hit;
+      } else if(a_max > b_max){
         i_b = next_i_b;
         v_b = _mm_loadu_si128((__m128i*)&B[i_b]);
         b_max = _mm_extract_epi32(v_b, 3);
         v_a = _mm_loadu_si128((__m128i*)&C[count+(INTS_PER_REG-num_hit)]);
+        count += INTS_PER_REG-num_hit;
+      } else{
+        i_a = next_i_a;
+        v_a = _mm_loadu_si128((__m128i*)&A[i_a]);
+        new_a_max = _mm_extract_epi32(v_a, 3);
+        i_b = next_i_b;
+        v_b = _mm_loadu_si128((__m128i*)&B[i_b]);
+        b_max = _mm_extract_epi32(v_b, 3);
+        count += INTS_PER_REG + (num_hit==1)*(INTS_PER_REG-num_hit);
       }
       a_max = new_a_max;
-      count += INTS_PER_REG-num_hit;// a number of elements is a weight of the mask
-      cout << "count: " << count << endl;
+      // a number of elements is a weight of the mask
+      //cout << "i_a: " << i_a  << " i_b: " << i_b << endl; 
     }
     #endif
 
+    //cout << "count: " << count << " " << i_a  << " " << i_b << " " << (a_max == b_max) << endl;
     if(a_max < b_max && i_b < s_b){
       bool a = i_b < s_b && C[count] > B[i_b];
       bool b = (i_b+1) < s_b && C[count+1] > B[i_b+1];
@@ -296,20 +312,19 @@ namespace array32 {
       bool d = (i_b+3) < s_b && C[count+3] > B[i_b+3];
 
       size_t incr = a*1 + b*1 + c*1 + d*1;
-      cout << "incr: " << incr << endl;
       count += incr;
       i_b += incr;
-    }
-    if(a_max > b_max && i_a < s_a){
+    }else if(a_max > b_max && i_a < s_a){
       bool a = i_a < s_a && C[count] > A[i_a];
       bool b = (i_a+1) < s_a && C[count+1] > A[i_a+1];
       bool c = (i_a+2) < s_a && C[count+2] > A[i_a+2];
       bool d = (i_a+3) < s_a && C[count+3] > A[i_a+3];
 
       size_t incr = a*1 + b*1 + c*1 + d*1;
-      cout << "incr: " << incr << " " << _mm_extract_epi32(v_a,0) << endl;
       count += incr;
       i_a += incr;
+    } else{
+      count += (num_hit==1)*(INTS_PER_REG-num_hit);
     }
 
     // intersect the tail using scalar intersection
@@ -323,7 +338,7 @@ namespace array32 {
         ++i_b;
         notFinished = i_b < s_b;
       }
-      cout << A[i_a] << endl;
+      i_b += notFinished*(A[i_a]==B[i_b]);
       #if WRITE_VECTOR == 1
       C[count] = A[i_a];
       #endif

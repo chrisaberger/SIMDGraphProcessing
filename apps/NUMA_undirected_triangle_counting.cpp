4c191c63f40acad1e63deba8fb336c4b84aa46c3
Christopher Aberger <craberger@gmail.com>
2014-10-27 10:58:00 -0700

    Added in NUMA app.

---
+++ apps/NUMA_undirected_triangle_counting.cpp
@@ -0,0 +1,173 @@
+#include <atomic>
+
+#include "Matrix.hpp"
+#include "MutableGraph.hpp"
+#include "Node.hpp"
+
+namespace application{
+  //Matrix *graph;
+  long num_triangles = 0;
+  common::type graphType = common::ARRAY32;
+
+  inline bool myNodeSelection(unsigned int node){
+    (void)node;
+    return true;
+  }
+  inline bool myEdgeSelection(unsigned int node, unsigned int nbr){
+    return nbr < node;
+  }
+}
+
+/*long edgeApply(void* graph, unsigned int src, unsigned int dst) {
+  return graph->row_intersect(result, src, dst);
+}*/
+
+class Worker {
+private:
+  Node* node;
+  Matrix* local_graph;
+  int num_nodes;
+  std::atomic<long> triangles;
+  uint8_t *result;
+
+public:
+  static void* operator new(std::size_t sz) {
+    return ::operator new(sz);
+  }
+
+  static void* operator new(std::size_t sz, int32_t node) {
+    numa_set_preferred(node);
+    return numa_alloc_onnode(sz, node);
+  }
+
+  static void operator delete(void* ptr) {
+  }
+
+  Worker(int node, int num_nodes, MutableGraph* input_graph) {
+    numa_run_on_node(node);
+    numa_set_preferred(node);
+
+    this->node = new Node(node);
+    this->local_graph = new (node) Matrix(
+      node,
+      input_graph->out_neighborhoods,
+      input_graph->num_nodes, input_graph->num_edges,
+      &application::myNodeSelection,
+      &application::myEdgeSelection,
+      application::graphType);
+    this->num_nodes = num_nodes;
+    this->result = new uint8_t[input_graph->num_nodes];
+  }
+
+  Worker(int node, int num_nodes, Matrix* input_graph) {
+    this->node = new Node(node);
+    this->node->run_on();
+    this->local_graph = input_graph;
+    this->num_nodes = num_nodes;
+    this->triangles = 0;
+    this->result = new uint8_t[input_graph->matrix_size];
+  }
+
+  int edgeApply(unsigned int src, unsigned int dst) {
+    this->triangles += this->local_graph->row_intersect(this->result, src, dst);
+    return 0;
+  }
+
+  void run(int num_threads) {
+    using namespace std::placeholders;
+
+    this->triangles = 0;
+    this->node->run_on();
+
+    for(int i = 0; i < num_threads; i++) {
+       auto edge_fun = std::bind(&Worker::edgeApply, this, _1, _2);
+       auto col_fun = std::bind(&Matrix::reduce_column_in_row<int>, this->local_graph, _1, _2);
+       //auto thread_fun = std::bind(&Matrix::part_reduce_row_omp<int>, this->local_graph, std::cref(col_fun), std::cref(edge_fun), this->node->get_id(), this->num_nodes);
+       auto thread_fun = std::bind(&Matrix::part_reduce_row<int>, this->local_graph, std::cref(col_fun), std::cref(edge_fun), i + this->node->get_id() * num_threads, this->num_nodes * num_threads);
+       std::thread* worker_thread = new std::thread(thread_fun);
+       this->node->add_thread(worker_thread);
+    }
+
+    cout << "Launched " << num_threads << " threads on node " << this->node->get_id() << endl;
+  }
+
+  long join() {
+     this->node->join_threads();
+     return this->triangles;
+  }
+};
+
+//Ideally the user shouldn't have to concern themselves with what happens down here.
+int main (int argc, char* argv[]) {
+  if(argc != 3){
+    cout << "Please see usage below: " << endl;
+    cout << "\t./main <adjacency list file/folder> <# of threads per node>" << endl;
+    exit(0);
+  }
+
+  ofstream outfile;
+  outfile.open("times.txt");
+
+  int num_threads = atoi(argv[2]);
+
+  int num_nodes = 1;
+  if(numa_available() < 0) {
+     cout << "Warning: NUMA API not supported" << endl;
+  }
+  else {
+     num_nodes = numa_max_node() + 1;
+     cout << "NUMA API supported" << endl;
+     cout << "Number of nodes: " << num_nodes << endl;
+  }
+
+  numa_run_on_node(2);
+
+  // Allocate memory where the thread is running
+  //numa_set_localalloc();
+
+
+  common::startClock();
+  MutableGraph inputGraph = MutableGraph::undirectedFromAdjList(argv[1],1); //filename, # of files
+
+  common::stopClock("Reading File");
+
+  common::startClock();
+
+  Matrix* mat = new Matrix(
+      0,
+      inputGraph.out_neighborhoods,
+      inputGraph.num_nodes, inputGraph.num_edges,
+      &application::myNodeSelection,
+      &application::myEdgeSelection,
+      application::graphType);
+
+  cout << "Number of nodes: " << mat->matrix_size << endl;
+
+  std::vector<Worker*> nodes;
+  for(int32_t i = 0; i < num_nodes; i++) {
+    nodes.push_back(new (i) Worker(i, num_nodes, &inputGraph));
+    //nodes.push_back(new Worker(i, num_nodes, mat));
+  }
+
+  common::stopClock("Building Graph");
+
+  for(int i = 1; i <= 24; i++) {
+     common::startClock();
+     for(auto worker : nodes) {
+       worker->run(i);
+     }
+
+     long sum = 0;
+     for(auto worker : nodes) {
+       sum += worker->join();
+     }
+     cout << "Triangles: " << sum << endl;
+     cout << i << endl;
+
+     double time = common::stopClock("CSR TRIANGLE COUNTING");
+     outfile << i << "\t" << time << endl;
+  }
+
+  outfile.close();
+  return 0;
+}

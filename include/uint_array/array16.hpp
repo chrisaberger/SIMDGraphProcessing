12d01f6900b2940c889a674cb322bc1225115806
Christopher Aberger <craberger@gmail.com>
2014-11-05 21:50:58 -0800

    Revert a16 back to it's own output.

--- include/uint_array/array16.hpp
+++ include/uint_array/array16.hpp
@@ -5,44 +5,6 @@ namespace array16 {
   static inline int getBitSD(unsigned int value, unsigned int position) {
     return ( ( value & (1 << position) ) >> position);
   }
-  static char max = char(0xff);
-  static __m128i shuffle_mask16_low[16] = {        
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max), //0
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,1,0), //1
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,3,2), //2
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,3,2,max,max,1,0), //3
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,5,4), //4
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,5,4,max,max,1,0), //5
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,5,4,max,max,3,2), //6
-    _mm_set_epi8(max,max,max,max,max,max,5,4,max,max,3,2,max,max,1,0), //7
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,7,6), //8
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,7,6,max,max,1,0), //9
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,7,6,max,max,3,2), //10
-    _mm_set_epi8(max,max,max,max,max,max,7,6,max,max,3,2,max,max,0,1), //11
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,7,6,max,max,5,4), //12
-    _mm_set_epi8(max,max,max,max,max,max,7,6,max,max,5,4,max,max,1,0), //13
-    _mm_set_epi8(max,max,max,max,max,max,7,6,max,max,5,4,max,max,3,2), //14
-    _mm_set_epi8(max,max,7,6,max,max,5,4,max,max,3,2,max,max,1,0) //15
-  }; 
-  static __m128i shuffle_mask16_high[16] = {        
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max), //0
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,9,8), //1
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,11,10), //2
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,11,10,max,max,9,8), //3
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,13,12), //4
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,13,12,max,max,9,8), //5
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,13,12,max,max,11,10), //6
-    _mm_set_epi8(max,max,max,max,max,max,13,12,max,max,11,10,max,max,9,8), //7
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,max,max,max,max,15,14), //8
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,15,14,max,max,9,8), //9
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,15,14,max,max,11,10), //10
-    _mm_set_epi8(max,max,max,max,max,max,15,14,max,max,11,10,max,max,9,8), //11
-    _mm_set_epi8(max,max,max,max,max,max,max,max,max,max,15,14,max,max,13,12), //12
-    _mm_set_epi8(max,max,max,max,max,max,15,14,max,max,13,12,max,max,9,8), //13
-    _mm_set_epi8(max,max,max,max,max,max,15,14,max,max,13,12,max,max,11,10), //14
-    _mm_set_epi8(max,max,15,14,max,max,13,12,max,max,11,10,max,max,9,8) //15
-  }; 
-
   static inline void prepare_shuffling_dictionary16() {
     //Number of bits that can possibly be set are the lower 8
     for(unsigned int i = 0; i < 256; i++) { // 2^8 possibilities we need to store masks for
@@ -85,7 +47,7 @@ namespace array16 {
 	  return counter*2;
 	}
 
-	inline size_t simd_intersect_vector16(unsigned int prefix, unsigned int *C, const unsigned short *A, const unsigned short *B, const size_t s_a, const size_t s_b) {
+	inline size_t simd_intersect_vector16(unsigned short *C, const unsigned short *A, const unsigned short *B, const size_t s_a, const size_t s_b) {
 	  #if WRITE_VECTOR == 0
     (void)C;
     #endif
@@ -97,8 +59,6 @@ namespace array16 {
 	  size_t st_a = (s_a / SHORTS_PER_REG) * SHORTS_PER_REG;
 	  size_t st_b = (s_b / SHORTS_PER_REG) * SHORTS_PER_REG;
  
-    __m128i prefix_reg = _mm_set1_epi32(prefix);
-
 	  while(i_a < st_a && i_b < st_b) {
 	    __m128i v_a = _mm_loadu_si128((__m128i*)&A[i_a]);
 	    __m128i v_b = _mm_loadu_si128((__m128i*)&B[i_b]);    
@@ -110,26 +70,15 @@ namespace array16 {
 	   //         _SIDD_UWORD_OPS|_SIDD_CMP_EQUAL_ANY|_SIDD_BIT_MASK);
 	    __m128i res_v = _mm_cmpestrm(v_b, SHORTS_PER_REG, v_a, SHORTS_PER_REG,
 	            _SIDD_UWORD_OPS|_SIDD_CMP_EQUAL_ANY|_SIDD_BIT_MASK);
-	    
-      unsigned int r = _mm_extract_epi32(res_v, 0);
+	    unsigned int r = _mm_extract_epi32(res_v, 0);
 
       #if WRITE_VECTOR == 1
-      unsigned int r_lower = r & 0x00ff;
-	    __m128i p = _mm_shuffle_epi8(v_a, shuffle_mask16_low[r_lower]);
-      p = _mm_or_si128(p,prefix_reg);
+	    __m128i p = _mm_shuffle_epi8(v_a, shuffle_mask16[r]);
 	    _mm_storeu_si128((__m128i*)&C[count], p);
-      count += _mm_popcnt_u32(r_lower);
-
-      unsigned int r_upper = ((r & 0xff00) >> 8);
-      p = _mm_shuffle_epi8(v_a, shuffle_mask16_low[r_upper]);
-      p = _mm_or_si128(p,prefix_reg);
-      _mm_storeu_si128((__m128i*)&C[count], p);
-      count += _mm_popcnt_u32(r_upper);
-	   #else
-      prefix_reg = prefix_reg;
-	    count += _mm_popcnt_u32(r);
 	   #endif
 
+	    count += _mm_popcnt_u32(r);
+	    
 	    i_a += (a_max <= b_max) * SHORTS_PER_REG;
 	    i_b += (a_max >= b_max) * SHORTS_PER_REG;
 	  }
@@ -145,7 +94,7 @@ namespace array16 {
 	    }
 	    if(notFinished && A[i_a] == B[i_b]){
         #if WRITE_VECTOR == 1
-	      C[count] = (prefix | (unsigned int) A[i_a]);
+	      C[count] = A[i_a];
         #endif
 	      ++count;
 	    }
@@ -154,8 +103,9 @@ namespace array16 {
 	  }
 	  return count;
 	}
-	inline size_t intersect(unsigned int *C,const unsigned short *A, const unsigned short *B, const size_t s_a, const size_t s_b) {
+	inline size_t intersect(unsigned short *C,const unsigned short *A, const unsigned short *B, const size_t s_a, const size_t s_b) {
 	  size_t i_a = 0, i_b = 0;
+	  size_t counter = 0;
 	  size_t count = 0;
 	  bool notFinished = i_a < s_a && i_b < s_b;
 
@@ -172,12 +122,18 @@ namespace array16 {
 	    } else {
 	      unsigned short partition_size = 0;
 	      //If we are not in the range of the limit we don't need to worry about it.
-        unsigned int prefix = ((unsigned int)(A[i_a])) << 16;
-        partition_size = simd_intersect_vector16(prefix,&C[count],&A[i_a + 2],&B[i_b + 2],A[i_a + 1], B[i_b + 1]);
+	      #if WRITE_VECTOR == 1
+        C[counter++] = A[i_a]; // write partition prefix
+	      #endif
+        partition_size = simd_intersect_vector16(&C[counter+1],&A[i_a + 2],&B[i_b + 2],A[i_a + 1], B[i_b + 1]);
+	      #if WRITE_VECTOR == 1
+        C[counter++] = partition_size; // write partition size
+	      #endif
         i_a += A[i_a + 1] + 2;
 	      i_b += B[i_b + 1] + 2;      
 
 	      count += partition_size;
+	      counter += partition_size;
 	      notFinished = i_a < s_a && i_b < s_b;
 	    }
 	  }

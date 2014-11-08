8b82de591134d420be154264328d31af91ce22fb
Andres Noetzli <noetzli@stanford.edu>
2014-11-07 16:02:53 -0800

    Minor update of clique counting

--- benchmarking/graphx_clique_counting/src/main/scala/CliqueCounting.scala
+++ benchmarking/graphx_clique_counting/src/main/scala/CliqueCounting.scala
@@ -79,7 +79,7 @@ object CliqueCounting {
       .setAppName("GraphX Clique Counting")
       .set("spark.executor.memory", "2g")
     val sc = new SparkContext(conf)
-    val edges = GraphLoader.edgeListFile(sc, "/dfs/scratch0/noetzli/datasets/california/edgelist/data.txt")
+    val edges = GraphLoader.edgeListFile(sc, args(0))
     val triangles = run(edges)
     val result = triangles.vertices.reduce((a: (VertexId, Int), b: (VertexId, Int)) => (a._1, a._2 + b._2))
     println(result._2)

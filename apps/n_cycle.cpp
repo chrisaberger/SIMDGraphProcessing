0d4a00e4544c0e05415fe0481434c1d0fd7393c8
Christopher Aberger <craberger@gmail.com>
2014-11-08 15:42:33 -0800

    n_cycle app committed.

---
+++ apps/n_cycle.cpp
@@ -0,0 +1,148 @@
+// class templates
+#include "AOA_Matrix.hpp"
+#include <cstdarg>
+#include "MutableGraph.hpp"
+#include "Table.hpp"
+
+namespace application{
+  AOA_Matrix *graph;
+  long num_triangles = 0;
+  Table *output;
+  size_t num_threads;
+  
+  inline bool myNodeSelection(unsigned int node){
+    (void)node;
+    return true;
+  }
+  inline bool myEdgeSelection(unsigned int node, unsigned int nbr){
+    return nbr < node;
+  }
+  inline long edgeApply(size_t depth, size_t query_depth, size_t thread_id, long t_count, uint8_t *buffer1, uint8_t *buffer2, Table *output, unsigned int *src_nbrhood, unsigned int src, unsigned int dst){
+    size_t index = (depth-1) + query_depth*thread_id;
+    long count = 0;
+    if(depth == 3){
+      output->tuple[query_depth*thread_id] = src;
+      output->tuple[query_depth*thread_id+1] = dst;
+    } else{
+      //intersect buffer2 with dst neighborhood
+      output->tuple[index-1] = dst;
+    }
+
+    if(depth == query_depth){
+      count = graph->row_intersect(buffer1,output->tuple[query_depth*thread_id],dst,src_nbrhood);
+      size_t cur_size = output->table_size[thread_id];
+      unsigned int *output_table = (output->table_pointers[index])+cur_size;
+      uint_array::decode(output_table,buffer1,count);
+      for(long i = 0; i < count; i++){
+        for(size_t j = 0; j < output->num_tuples-1; j++){ //the last row is taken care of in decode
+          unsigned int *tmp_row = output->table_pointers[query_depth*thread_id+j];
+          tmp_row[cur_size+i] = output->tuple[query_depth*thread_id+j];
+        }
+      }
+      output->table_size[thread_id] += count;   
+    } else {
+      auto ef = std::bind(&edgeApply,depth+1,query_depth,thread_id,count,buffer2,buffer1,output,src_nbrhood,_1,_2);
+      auto row_function = std::bind(&AOA_Matrix::sum_over_columns_in_row<long>, graph, _1, _2, _3);
+  
+      count = (row_function)(dst,(unsigned int*)buffer1,ef);
+    }
+
+    return count;
+  }
+  inline void queryOver(const size_t query_depth){
+    auto row_function = std::bind(&AOA_Matrix::sum_over_columns_in_row<long>, graph, _1, _2, _3);
+
+    const size_t matrix_size = graph->matrix_size;
+    const size_t cardinality = graph->cardinality;
+
+    output = new Table(query_depth,num_threads,cardinality);
+
+    long reducer = 0;
+    
+    #pragma omp parallel default(none) shared(row_function,output,num_threads) reduction(+:reducer) 
+    {
+      uint8_t *t_local_buffer_1 = new uint8_t[(cardinality*4)/(num_threads)];
+      uint8_t *t_local_buffer_2 = new uint8_t[(cardinality*4)/(num_threads)];
+
+      unsigned int *t_local_decode = new unsigned int[matrix_size];
+      long t_local_reducer = 0;
+      
+      #pragma omp for schedule(dynamic)
+      for(size_t i = 0; i < matrix_size; i++){
+        auto edge_function = std::bind(&edgeApply,3,query_depth,omp_get_thread_num(),t_local_reducer,t_local_buffer_1,t_local_buffer_2,output,t_local_decode,_1,_2);
+        t_local_reducer += (row_function)(i,t_local_decode,edge_function);
+      }
+      reducer += t_local_reducer;
+
+      delete[] t_local_buffer_1;
+      delete[] t_local_buffer_2;
+      delete[] t_local_decode;
+    }
+
+    num_triangles = reducer;
+  }
+}
+
+//Ideally the user shouldn't have to concern themselves with what happens down here.
+int main (int argc, char* argv[]) { 
+  if(argc != 5){
+    cout << "Please see usage below: " << endl;
+    cout << "\t./main <adjacency list file/folder> <# of threads> <layout type=bs,a16,a32,hybrid,v,bp> <depth>" << endl;
+    exit(0);
+  }
+
+  cout << endl << "Number of threads: " << atoi(argv[2]) << endl;
+  omp_set_num_threads(atoi(argv[2]));        
+  application::num_threads = atoi(argv[2]);
+
+  std::string input_layout = argv[3];
+
+  common::type layout;
+  if(input_layout.compare("a32") == 0){
+    layout = common::ARRAY32;
+  } else if(input_layout.compare("bs") == 0){
+    layout = common::BITSET;
+  } else if(input_layout.compare("a16") == 0){
+    layout = common::ARRAY16;
+  } else if(input_layout.compare("hybrid") == 0){
+    layout = common::HYBRID_PERF;
+  } else if(input_layout.compare("v") == 0){
+    layout = common::VARIANT;
+  } else if(input_layout.compare("bp") == 0){
+    layout = common::A32BITPACKED;
+  } else{
+    cout << "No valid layout entered." << endl;
+    exit(0);
+  }
+
+  auto node_selection = std::bind(&application::myNodeSelection, _1);
+  auto edge_selection = std::bind(&application::myEdgeSelection, _1, _2);
+
+  common::startClock();
+  MutableGraph *inputGraph = MutableGraph::undirectedFromEdgeList(argv[1]); //filename, # of files
+  common::stopClock("Reading File");
+  
+  common::startClock();
+  inputGraph->reorder_by_degree();
+  common::stopClock("Reordering");
+  
+  cout << endl;
+
+  common::startClock();
+  application::graph = AOA_Matrix::from_symmetric(inputGraph->out_neighborhoods,
+    inputGraph->num_nodes,inputGraph->num_edges,
+    node_selection,edge_selection,inputGraph->external_ids,layout);
+  common::stopClock("selections");
+  
+  inputGraph->MutableGraph::~MutableGraph(); 
+  
+  common::startClock();
+  application::queryOver(atoi(argv[4]));
+  common::stopClock(input_layout);  
+
+  //application::graph->AOA_Matrix::~AOA_Matrix();
+  cout << "Count: " << application::num_triangles << endl << endl;
+
+  application::output->print_data("table.txt");
+  return 0;
+}

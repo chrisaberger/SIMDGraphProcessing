1b37398bf1646f9e1fb72091a31345434bc42ed5
Andres Noetzli <noetzli@stanford.edu>
2014-11-21 11:33:08 -0800

    test code added to postgres path counting

--- benchmarking/postgres/path_counting.sql
+++ benchmarking/postgres/path_counting.sql
@@ -7,4 +7,6 @@ SELECT COUNT(*) FROM edge e1, edge e2, edge e3, edge e4 WHERE
    e3.a != e4.a AND e3.a != e4.b AND
    e4.a != e4.b;
 
+-- SELECT COUNT(*) FROM edge e1, edge e2 WHERE
+--   e1.a = 88160 AND e1.b = e2.a AND e1.a != e2.b;
 \timing off

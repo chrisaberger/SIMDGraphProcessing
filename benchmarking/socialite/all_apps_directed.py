aeb2df03af6706a15beaadcc494dbde4a1d15d0d
Andres Noetzli <noetzli@stanford.edu>
2014-12-01 16:18:22 -0800

    update to socialite benchmarks

--- benchmarking/socialite/all_apps_directed.py
+++ benchmarking/socialite/all_apps_directed.py
@@ -8,17 +8,23 @@ start = time.time()
 filename = sys.argv[1]
 
 print "Loading data (directed)"
-`edge(int a:0..2000000, (int b)).
- edge(a, b) :- l = $read($filename), (v1,v2) = $split(l, " "), a = $toInt(v1), b = $toInt(v2).`
+`edge(int a:0..20000000, (int b)).
+ edge(a, b) :- l = $read($filename), (v1,v2) = $split(l, "\t"), a = $toInt(v1), b = $toInt(v2).`
 
 print "4-path"
 start = time.time()
+
+`path(int a:0..20000000, int d).
+ total(int x:0..0, int s).`
+
 for k in range(0, num_runs):
     print "Run " + str(k)
-    `path(int a:0..2000000, int d).
+    `clear path.
+     clear total.
      path(b, $min(d)) :- edge(222074, b), d = 1;
-                      :- path(a, d1), edge(a, b), d = d1 + 1.`
+                      :- path(a, d1), d1 < 4, edge(a, b), d = d1 + 1.
+     total(0, $sum(1)) :- path(x, 4).`
 
 print "4-path time: " + str((time.time() - start) / num_runs)
 for i, s in `total(i, s)`:
-    print "4-path: " + str(s / num_runs)
+    print "4-path: " + str(s)

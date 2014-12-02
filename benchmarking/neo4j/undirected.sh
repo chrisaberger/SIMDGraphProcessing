07dd917b57c1890b2f76e022ff3196ea922f4f46
Andres Noetzli <noetzli@stanford.edu>
2014-12-02 02:05:39 -0800

    update to neo4j benchmarking

--- benchmarking/neo4j/undirected.sh
+++ benchmarking/neo4j/undirected.sh
@@ -10,7 +10,7 @@ ${NEO4J_HOME}/bin/neo4j start
 echo "Loading data"
 ${NEO4J_HOME}/bin/neo4j-shell << EOF
 CREATE CONSTRAINT ON (node:AwesomeNode) ASSERT node.node_id IS UNIQUE;
-USING PERIODIC COMMIT LOAD CSV FROM 'file://$1' AS line FIELDTERMINATOR ' ' MERGE (fromNode:AwesomeNode { node_id: toInt(line[0]) }) MERGE (toNode:AwesomeNode { node_id: toInt(line[1]) }) CREATE (fromNode)-[:AwesomeEdge]->(toNode);
+USING PERIODIC COMMIT LOAD CSV FROM 'file://$1/glab_undirected/data.txt' AS line FIELDTERMINATOR ' ' MERGE (fromNode:AwesomeNode { node_id: toInt(line[0]) }) MERGE (toNode:AwesomeNode { node_id: toInt(line[1]) }) CREATE (fromNode)-[:AwesomeEdge]->(toNode);
 EOF
 
 echo "Triangle counting"
@@ -26,9 +26,4 @@ EOF
 echo "Cycle counting"
 ${NEO4J_HOME}/bin/neo4j-shell << EOF
 MATCH (n:AwesomeNode)-[:AwesomeEdge*3]->(n3:AwesomeNode)<-[:AwesomeEdge]-(n) RETURN COUNT(n);
-EOF
-
-echo "Path counting"
-${NEO4J_HOME}/bin/neo4j-shell << EOF
-MATCH (node:AwesomeNode {node_id: 88160})-[*2]->(node2) RETURN COUNT(DISTINCT node2);
 EOF

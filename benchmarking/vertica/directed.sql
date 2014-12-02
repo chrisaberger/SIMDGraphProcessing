a83430d55382d1daf4d5f0bb74ca9b52270f61ff
Andres Noetzli <noetzli@stanford.edu>
2014-12-01 16:47:54 -0800

    fix for vertica path counting

--- benchmarking/vertica/directed.sql
+++ benchmarking/vertica/directed.sql
@@ -11,11 +11,17 @@ copy edges from :file direct delimiter ' ';
 DROP VIEW frontier1;
 DROP VIEW frontier2;
 DROP VIEW frontier3;
+DROP VIEW visited1;
+DROP VIEW visited2;
+DROP VIEW visited3;
 
 \timing
 -- Path counting
 CREATE VIEW frontier1 AS SELECT e1.dest AS v FROM edges e1 WHERE e1.source = 222074;
-CREATE VIEW frontier2 AS SELECT e2.dest AS v FROM frontier1 JOIN edges e2 ON frontier1.v = e2.source WHERE e2.dest NOT IN (SELECT * FROM frontier1);
-CREATE VIEW frontier3 AS SELECT e3.dest AS v FROM frontier2 JOIN edges e3 ON frontier2.v = e3.source WHERE e3.dest != 222074 AND e3.dest NOT IN (SELECT * FROM frontier1 UNION SELECT * FROM frontier2);
-SELECT COUNT(*) FROM frontier3 JOIN edges e4 ON frontier3.v = e4.source WHERE e4.dest != 222074 AND e4.dest NOT IN (SELECT * FROM frontier1 UNION SELECT * FROM frontier2 UNION SELECT * FROM frontier3);
+CREATE VIEW visited1 AS SELECT 222074 UNION SELECT * FROM frontier1;
+CREATE VIEW frontier2 AS SELECT e2.dest AS v FROM frontier1 JOIN edges e2 ON frontier1.v = e2.source WHERE e2.dest NOT IN (SELECT * FROM visited1);
+CREATE VIEW visited2 AS SELECT * FROM visited1 UNION SELECT * FROM frontier2;
+CREATE VIEW frontier3 AS SELECT e3.dest AS v FROM frontier2 JOIN edges e3 ON frontier2.v = e3.source WHERE e3.dest NOT IN (SELECT * FROM visited2);
+CREATE VIEW visited3 AS SELECT * FROM visited2 UNION SELECT * FROM frontier3;
+SELECT COUNT(DISTINCT e4.dest) FROM frontier3 JOIN edges e4 ON frontier3.v = e4.source WHERE e4.dest NOT IN (SELECT * FROM visited3);
 \timing

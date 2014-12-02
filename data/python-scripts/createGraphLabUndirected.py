fe17c5bad35ed312d223d473e86f378a0e334d98
Andres Noetzli <noetzli@stanford.edu>
2014-12-01 20:15:45 -0800

    update to python tool to create glab files

--- data/python-scripts/createGraphLabUndirected.py
+++ data/python-scripts/createGraphLabUndirected.py
@@ -1,16 +1,24 @@
-#f = open('simple.txt','r')
 import sys
-read_file = sys.argv[1]
 
-f = open(read_file,'r')
-w2 = open('graphlab/' + read_file,'w')
+read_file = sys.argv[1]
+out_file = sys.argv[2]
+
+sep = None
+if len(sys.argv) > 3:
+    sep = sys.argv[3]
+
+f = open(read_file, 'r')
+w2 = open(out_file, 'w')
 i = 0
 j = 0
 s = set([])
+
 for line in f:
-	nodes = line.split()
-	if( (nodes[0] != '%') and  ((int(nodes[0]),int(nodes[1])) not in s) and ((int(nodes[1]),int(nodes[0])) not in s)):
-		w2.write(nodes[0] + " " + nodes[1] + '\n')
-		s.add((int(nodes[0]),int(nodes[1])))
+    nodes = line.split() if not sep else line.split(sep)
+    source = int(nodes[0])
+    dest = int(nodes[1])
+    if((source != '%') and ((source, dest) not in s) and ((dest, source) not in s)):
+        w2.write(" ".join(nodes) + '\n')
+        s.add((source, dest))
 w2.close()
 f.close()

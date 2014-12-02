370cabbf00db27d6ccae8473638fbd98959b7469
Andres Noetzli <noetzli@stanford.edu>
2014-12-01 20:22:29 -0800

    Small python tool to change the seperator of a data file

---
+++ data/python-scripts/changeSep.py
@@ -0,0 +1,17 @@
+import sys
+
+read_file = sys.argv[1]
+out_file = sys.argv[2]
+
+sep = None
+if len(sys.argv) > 3:
+    sep = sys.argv[3]
+
+f = open(read_file, 'r')
+w = open(out_file, 'w')
+
+for line in f:
+    nodes = line.split() if not sep else line.split(sep)
+    w.write(" ".join(nodes) + '\n')
+w.close()
+f.close()

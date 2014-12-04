d89effbfc78c670a9d63d71f101cdfe09ad62cc0
Andres Noetzli <noetzli@stanford.edu>
2014-12-04 14:35:43 -0800

    Workaround for bug in socialite and bug fix in query

--- benchmarking/socialite/selections_directed.py
+++ benchmarking/socialite/selections_directed.py
@@ -16,12 +16,11 @@ edgelist_file = os.path.join(path, "edgelist", "data.txt")
 
 print "Loading data"
 `edge(long a, (long b, int year)) indexby a, sortby b.
- edgeRaw(long a, (long b, int year)) indexby a, sortby b.
  node(long n, int place_id) indexby n.
  node(n, place_id) :- l = $read($attributes_file), (v1, v2) = $split(l, " "), n = $toLong(v1), place_id = $toInt(v2).
- edgeRaw(a, b, year) :- l = $read($edgelist_file), (v1, v2, v3) = $split(l, " "), a = $toLong(v1), b = $toLong(v2), year = $getYear(v3).`
+ edge(a, b, year) :- l = $read($edgelist_file), (v1, v2, v3) = $split(l, " "), a = $toLong(v1), b = $toLong(v2), year = $getYear(v3).`
 
-`path(long a:0..20000000, int d).
+`path(long a, int d).
  total(int x:0..0, int s).
  filtered_edge(long a, (long b)) indexby a, sortby b.
  filtered_node(long n) indexby n.`
@@ -32,7 +31,15 @@ for k in range(0, num_runs):
      clear total.
      clear filtered_edge.
      clear filtered_node.
-     path(b, $min(d)) :- filtered_edge(222074L, b), d = 1;
+     filtered_node(n) :- node(n, p), p > 500.
+     filtered_edge(frm, to) :- edge(frm, to, year), year == 2012, filtered_node(frm), filtered_node(to).
+     out_deg(long a, int b).
+     max_out(int x:0..0, int b).
+     maxnode(long a).
+     out_deg(a, $sum(1)) :- filtered_edge(a, b).
+     max_out(0, $max(b)) :- out_deg(a, b).
+     maxnode(a) :- max_out(0, b), out_deg(a, b).
+     path(b, $min(d)) :- filtered_edge(14293652286639L, b), d = 1;
                       :- path(a, d1), d1 < 4, filtered_edge(a, b), d = d1 + 1.
      total(0, $sum(1)) :- path(x, 4).`
 

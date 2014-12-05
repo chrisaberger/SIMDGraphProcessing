7f15f52148e57a3c7442de95ccaaf81ab2f58ce9
Andres Noetzli <noetzli@stanford.edu>
2014-12-04 22:20:46 -0800

    fix in socialite benchmarking

--- benchmarking/neo4j/selections_undirected.sh
+++ benchmarking/neo4j/selections_undirected.sh
@@ -10,12 +10,16 @@ ${NEO4J_HOME}/bin/neo4j start
 echo "Loading data"
 ${NEO4J_HOME}/bin/neo4j-shell << EOF
 CREATE CONSTRAINT ON (node:AwesomeNode) ASSERT node.node_id IS UNIQUE;
+CREATE INDEX ON :AwesomeNode(location);
+CREATE INDEX ON :AwesomeEdge(year);
 USING PERIODIC COMMIT LOAD CSV FROM 'file://$1/attributes.txt' AS line FIELDTERMINATOR ' ' CREATE (node:AwesomeNode { node_id: toInt(line[0]), location: toInt(line[1]) });
 EOF
 
 ${NEO4J_HOME}/bin/neo4j-shell << EOF
 USING PERIODIC COMMIT LOAD CSV FROM 'file://$1/glab_undirected/data.txt' AS line FIELDTERMINATOR ' ' MERGE (fromNode:AwesomeNode { node_id: toInt(line[0]) }) MERGE (toNode:AwesomeNode { node_id: toInt(line[1]) }) CREATE (fromNode)-[:AwesomeEdge { year: toInt(substring(line[2], 0, 4)) }]->(toNode);
 EOF
+
+${NEO4J_HOME}/bin/neo4j-shell
 
 echo "Triangle counting"
 ${NEO4J_HOME}/bin/neo4j-shell << EOF
--- benchmarking/socialite/all_apps_undirected.py
+++ benchmarking/socialite/all_apps_undirected.py
@@ -9,43 +9,45 @@ start = time.time()
 filename = os.path.join(sys.argv[1], "glab_undirected", "data.txt")
 
 print "Loading data (undirected)"
-`edge(int a:0..20000000, (int b))  indexby a, sortby b.
- edgeRaw(int a:0..20000000, (int b))  indexby a, sortby b.
+`edge(int a:0..20000000, (int b)) indexby a, sortby b.
+ edgeRaw(int a:0..20000000, (int b)) indexby a, sortby b.
  edgeRaw(a, b) :- l = $read($filename), (v1,v2) = $split(l, " "), a = $toInt(v1), b = $toInt(v2).`
 
 print "Preprocessing data"
 `edge(a, b) :- edgeRaw(a, b), a < b.
  edge(b, a) :- edgeRaw(a, b), b < a.`
 
+`total(int x:0..0, int s).`
+
 print "Triangle counting"
 start = time.time()
 for k in range(0, num_runs):
     print "Run " + str(k)
-    `total(int x:0..0, int s).
+    `clear total.
      total(0, $sum(1)) :- edge(x, y), edge(y, z), edge(x, z).`
 
 print "Triangle time: " + str((time.time() - start) / num_runs)
 for i, s in `total(i, s)`:
-    print "Triangles: " + str(s / num_runs)
+    print "Triangles: " + str(s)
 
 print "4-clique counting"
 start = time.time()
 for k in range(0, num_runs):
     print "Run " + str(k)
-    `total(int x:0..0, int s).
+    `clear total.
      total(0, $sum(1)) :- edge(x, y), edge(y, z), edge(z, w), edge(x, w), edge(x, z), edge(y, w).`
 
 print "4-clique time: " + str((time.time() - start) / num_runs)
 for i, s in `total(i, s)`:
-    print "4-cliques: " + str(s / num_runs)
+    print "4-cliques: " + str(s)
 
 print "4-cycle counting"
 start = time.time()
 for k in range(0, num_runs):
     print "Run " + str(k)
-    `total(int x:0..0, int s).
+    `clear total.
      total(0, $sum(1)) :- edge(x, y), edge(y, z), edge(z, w), edge(x, w).`
 
 print "4-cycles time: " + str((time.time() - start) / num_runs)
 for i, s in `total(i, s)`:
-    print "4-cycles: " + str(s / num_runs)
+    print "4-cycles: " + str(s)

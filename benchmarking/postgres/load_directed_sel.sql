df3eaa66fe65579d1d3bb17d985db1503dbd4e6d
Andres Noetzli <noetzli@stanford.edu>
2014-12-08 18:11:56 -0800

    minor fix in postgres loader

--- benchmarking/postgres/load_directed_sel.sql
+++ benchmarking/postgres/load_directed_sel.sql
@@ -1,22 +1,16 @@
 DROP TABLE IF EXISTS edge;
 DROP TABLE IF EXISTS node;
 DROP TABLE IF EXISTS edgeRaw;
-DROP TABLE IF EXISTS nodeRaw;
 
 CREATE TABLE edgeRaw(a BIGINT, b BIGINT, year TIMESTAMP, PRIMARY KEY(a, b));
 CREATE TABLE edge(a BIGINT, b BIGINT, year INT, PRIMARY KEY(a, b));
-CREATE TABLE nodeRaw(a BIGINT, place INT, PRIMARY KEY(a));
 CREATE TABLE node(a BIGINT, place INT, PRIMARY KEY(a));
 
 COPY edgeRaw FROM :'edgelist' WITH DELIMITER ' ';
-COPY nodeRaw FROM :'attributes' WITH DELIMITER ' ';
+COPY node FROM :'attributes' WITH DELIMITER ' ';
  
 INSERT INTO edge(a, b, year) (
-   SELECT DISTINCT LEAST(a, b), GREATEST(a, b), EXTRACT(YEAR FROM year) FROM edgeRaw
-);
-
-INSERT INTO node(a, place) (
-   SELECT a, place FROM nodeRaw
+   SELECT e.a, e.b, EXTRACT(YEAR FROM year) FROM edgeRaw e
 );
 
 CREATE INDEX index_edge_a ON edge(a);

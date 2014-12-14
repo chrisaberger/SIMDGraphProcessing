9e30633fcfc4608b69be2ca36dcb93a3f0078791
Andres Noetzli <noetzli@stanford.edu>
2014-12-14 01:28:32 -0800

    missing files

---
+++ benchmarking/socialite/parse_output.py
@@ -0,0 +1,10 @@
+import re
+
+def getSocialitePerformance(f):
+  perf = []
+  for line in f:
+    matchObj = re.match(r'Running time : (\d+.\d*).*', line, re.M|re.I)
+    if matchObj:
+      perf.append(matchObj.group(1))
+
+  return {'perf':perf}

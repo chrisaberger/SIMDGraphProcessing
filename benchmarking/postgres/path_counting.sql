4eb260bf0e3ddfaeef27ffec2b2ea25f7bfdf6c4
Andres Noetzli <noetzli@stanford.edu>
2014-12-18 12:57:07 -0800

    updated version of postgres benchmarks

--- benchmarking/postgres/path_counting.sql
+++ benchmarking/postgres/path_counting.sql
@@ -1,10 +1,10 @@
-\timing on
 CREATE TEMPORARY VIEW frontier1 AS SELECT e1.b AS v FROM edge e1 WHERE e1.a = :start_node;
 CREATE TEMPORARY VIEW visited1 AS SELECT :start_node UNION SELECT * FROM frontier1;
 CREATE TEMPORARY VIEW frontier2 AS SELECT e2.b AS v FROM frontier1 JOIN edge e2 ON frontier1.v = e2.a WHERE e2.b NOT IN (SELECT * FROM visited1);
 CREATE TEMPORARY VIEW visited2 AS SELECT * FROM visited1 UNION SELECT * FROM frontier2;
 CREATE TEMPORARY VIEW frontier3 AS SELECT e3.b AS v FROM frontier2 JOIN edge e3 ON frontier2.v = e3.a WHERE e3.b NOT IN (SELECT * FROM visited2);
 CREATE TEMPORARY VIEW visited3 AS SELECT * FROM visited2 UNION SELECT * FROM frontier3;
+\timing on
 SELECT COUNT(DISTINCT e4.b) FROM frontier3 JOIN edge e4 ON frontier3.v = e4.a WHERE e4.b NOT IN (SELECT * FROM visited3);
 -- SELECT * FROM frontier1;
 -- SELECT * FROM frontier2;
--- benchmarking/postgres/run.sh
+++ benchmarking/postgres/run.sh
@@ -2,30 +2,34 @@
 
 PORT=5477
 
-if [ -f $1/attributes.txt ]; then
-   psql -p ${PORT} -v attributes="${1}/attributes.txt" -v edgelist="${1}/edgelist/data.txt" -f load_directed_sel.sql
-   for app in "path_counting"; do
-      echo "----- ${app} -----"
-      psql -p ${PORT} -v dataset=${1} -f ${app}_sel.sql
-   done
+if [ "${3}" -eq "1" ]; then
+  for i in `seq 1 ${1}`; do
+    if [ -f $1/attributes.txt ]; then
+       psql -p ${PORT} -v attributes="${2}/attributes.txt" -v edgelist="${2}/edgelist/data.txt" -f load_directed_sel.sql
+       for app in "path_counting"; do
+          echo "----- ${app} -----"
+          psql -p ${PORT} -v dataset=${2} -f ${app}_sel.sql
+       done
 
-   psql -p ${PORT} -v attributes="${1}/attributes.txt" -v edgelist="${1}/glab_undirected/data.txt" -f load_undirected_sel.sql
-   for app in "triangle_counting" "cycle_counting" "clique_counting"; do
-      echo "----- ${app} -----"
-      psql -p ${PORT} -f ${app}_sel.sql
-   done
+       psql -p ${PORT} -v attributes="${2}/attributes.txt" -v edgelist="${2}/glab_undirected/data.txt" -f load_undirected_sel.sql
+       for app in "triangle_counting" "clique_counting"; do
+          echo "----- ${app} -----"
+          psql -p ${PORT} -f ${app}_sel.sql
+       done
+    else
+       psql -p ${PORT} -v dataset="${2}/edgelist/data.txt" -f load_directed.sql
+       for app in "num_nbrs" "path_counting"; do
+          echo "----- ${app} -----"
+          psql -p ${PORT} -v dataset=${2} -v start_node=222074 -f ${app}.sql
+       done
+
+       psql -p ${PORT} -v dataset="${2}/glab_undirected/data.txt" -f load_undirected.sql
+       for app in "triangle_counting" "clique_counting"; do
+          echo "----- ${app} -----"
+          psql -p ${PORT} -v dataset=${2} -f ${app}.sql
+       done
+    fi
+  done
 else
-   psql -p ${PORT} -v dataset="${1}/edgelist/data.txt" -f load_directed.sql
-   for app in "num_nbrs" "path_counting"; do
-      echo "----- ${app} -----"
-      psql -p ${PORT} -v dataset=${1} -v start_node=222074 -f ${app}.sql
-   done
-
-   psql -p ${PORT} -v dataset="${1}/glab_undirected/data.txt" -f load_undirected.sql
-   for app in "triangle_counting" "cycle_counting" "clique_counting"; do
-      echo "----- ${app} -----"
-      psql -p ${PORT} -v dataset=${1} -f ${app}.sql
-   done
+  echo "Only single threaded execution supported"
 fi
-
-

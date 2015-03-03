f0800e023916ffaa0fa5bf3a5fe030eb0ee579cb
Andres Noetzli <noetzli@stanford.edu>
2015-03-03 13:20:12 -0800

    cleaned run.sh for logicblox

--- benchmarking/logicblox/run.sh
+++ benchmarking/logicblox/run.sh
@@ -2,29 +2,33 @@
 
 TIMEOUT=1800
 
-for i in `seq 1 ${1}`; do
-  if [ -f $1/attributes.txt ]; then
-    echo "Currently no queries with attributes"
-  else
-     export input_file="${2}/glab_undirected/data.txt"
-     envsubst < load_data.template > load_data.lb
+if [ -f $1/attributes.txt ]; then
+  echo "Queries with attributes are currently not supported"
+else
+  # Replace the ${input_file} variable in the data loading template with the
+  # actual input file name.
+  load_data_file=`tempfile`
+  export input_file="${1}/glab_undirected/data.txt"
+  envsubst < load_data.template > ${load_data_file}
 
-     lb create --overwrite benchmarking
-     lb addblock -f add_edges.lb benchmarking
-     lb exec -f load_data.lb benchmarking
+  # Load data
+  lb create --overwrite benchmarking
+  lb addblock -f add_edges.lb benchmarking
+  lb exec -f ${load_data_file} benchmarking
 
-     # for app in "cliqueCounting" "cycleCounting" "lollipopCounting"; do
-     for app in "triangleCounting" "cliqueCounting" "lollipopCounting"; do #"similarity" "symbiosity"; do
-        echo "----- ${app} -----"
-        time timeout $TIMEOUT lb exec --readonly --commit-mode softcommit -f ${app}.lb --print _count benchmarking
+  # Run queries
+  for app in "triangleCounting" "cliqueCounting" "lollipopCounting"; do #"similarity" "symbiosity"; do
+    echo "----- ${app} -----"
+    time timeout $TIMEOUT lb exec --readonly --commit-mode softcommit -f ${app}.lb --print _count benchmarking
 
-        tid=`lb status --active --debug benchmarking | awk '/request_id/ {print $2;}'`
+    # Check if the transaction is still running (because of a timeout) and
+    # abort it.
+    tid=`lb status --active --debug benchmarking | awk '/request_id/ {print $2;}'`
+    if [[ $tid ]]; then
+      lb aborttransaction benchmarking $tid
+    fi
+  done
 
-        if [[ $tid ]]; then
-          lb aborttransaction benchmarking $tid
-        fi
-     done
-
-     lb delete benchmarking
-  fi
-done
+  # Delete the workspace
+  lb delete benchmarking
+fi

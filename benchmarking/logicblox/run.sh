a242bd0abe6ffc822f3fa8058ef7c8c6b2d514cf
Andres Noetzli <noetzli@stanford.edu>
2015-03-03 13:06:23 -0800

    more robust handling of timeouts for logicblox, better file loading

--- benchmarking/logicblox/run.sh
+++ benchmarking/logicblox/run.sh
@@ -6,19 +6,23 @@ for i in `seq 1 ${1}`; do
   if [ -f $1/attributes.txt ]; then
     echo "Currently no queries with attributes"
   else
-     ln -f -s "${2}/glab_undirected/data.txt" input.txt
+     export input_file="${2}/glab_undirected/data.txt"
+     envsubst < load_data.template > load_data.lb
 
      lb create --overwrite benchmarking
      lb addblock -f add_edges.lb benchmarking
      lb exec -f load_data.lb benchmarking
 
      # for app in "cliqueCounting" "cycleCounting" "lollipopCounting"; do
-     for app in "triangleCounting" "cliqueCounting"; do #"similarity" "symbiosity"; do
+     for app in "triangleCounting" "cliqueCounting" "lollipopCounting"; do #"similarity" "symbiosity"; do
         echo "----- ${app} -----"
-        #./launch_server.sh
-        time timeout $TIMEOUT lb exec -f ${app}.lb benchmarking
-        #unit --sequential --threads $3 --time --test ${app}.lb
-        #./stop_server.sh
+        time timeout $TIMEOUT lb exec --readonly --commit-mode softcommit -f ${app}.lb --print _count benchmarking
+
+        tid=`lb status --active --debug benchmarking | awk '/request_id/ {print $2;}'`
+
+        if [[ $tid ]]; then
+          lb aborttransaction benchmarking $tid
+        fi
      done
 
      lb delete benchmarking

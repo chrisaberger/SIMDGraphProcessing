e10b0f760c82a79208d7a23de812633672520fdd
Andres Noetzli <noetzli@stanford.edu>
2015-03-03 12:15:38 -0800

    updated logicblox queries

--- benchmarking/logicblox/run.sh
+++ benchmarking/logicblox/run.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-TIMEOUT=9000
+TIMEOUT=1800
 
 for i in `seq 1 ${1}`; do
   if [ -f $1/attributes.txt ]; then
     echo "Currently no queries with attributes"
   else
      ln -f -s "${2}/glab_undirected/data.txt" input.txt
-     for app in "cliqueCounting" "cycleCounting" "lollipopCounting"; do
+
+     lb create --overwrite benchmarking
+     lb addblock -f add_edges.lb benchmarking
+     lb exec -f load_data.lb benchmarking
+
+     # for app in "cliqueCounting" "cycleCounting" "lollipopCounting"; do
+     for app in "triangleCounting" "cliqueCounting"; do #"similarity" "symbiosity"; do
         echo "----- ${app} -----"
-        ./launch_server.sh
-        timeout $TIMEOUT lb unit --sequential --threads $3 --time --test ${app}.lb
-        ./stop_server.sh
+        #./launch_server.sh
+        time timeout $TIMEOUT lb exec -f ${app}.lb benchmarking
+        #unit --sequential --threads $3 --time --test ${app}.lb
+        #./stop_server.sh
      done
+
+     lb delete benchmarking
   fi
 done

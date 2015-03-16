4927a092ff89163a7e83a46ab17adae020eed8d5
Andres Noetzli <noetzli@stanford.edu>
2015-03-16 14:08:47 -0700

    added missing test

---
+++ test/undirected_lollipop_counting_test.cpp
@@ -0,0 +1,13 @@
+#define WRITE_VECTOR 1
+
+#include "gtest/gtest.h"
+#include "undirected_lollipop_counting.cpp"
+
+TEST(CliqueTest, FacebookCliqueCounting) {
+  MutableGraph* inputGraph = MutableGraph::undirectedFromBinary("test/data/facebook.bin");
+  Parser input_data(4,false,0,0,inputGraph,"hybrid");
+  undirected_lollipop_counting<hybrid,hybrid> undirected_lollipop_counting_app(input_data);
+  undirected_lollipop_counting_app.run();
+  size_t expected_result = 713455740;
+  EXPECT_EQ(expected_result, undirected_lollipop_counting_app.num_lollipops);
+}

0629561837bc9d644c3e85b7438f98145c62b8bc
Andres Noetzli <noetzli@stanford.edu>
2015-03-16 14:24:36 -0700

    fixed typo

--- test/undirected_lollipop_counting_test.cpp
+++ test/undirected_lollipop_counting_test.cpp
@@ -3,7 +3,7 @@
 #include "gtest/gtest.h"
 #include "undirected_lollipop_counting.cpp"
 
-TEST(CliqueTest, FacebookCliqueCounting) {
+TEST(LollipopTest, FacebookLollipopCountingHybrid) {
   MutableGraph* inputGraph = MutableGraph::undirectedFromBinary("test/data/facebook.bin");
   Parser input_data(4,false,0,0,inputGraph,"hybrid");
   undirected_lollipop_counting<hybrid,hybrid> undirected_lollipop_counting_app(input_data);
